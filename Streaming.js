935f3ba794fa103c1e76e1c6313d01c155d8aa2e
Alex345-dev <36_tributo.chevrolet@icloud.com>
2025-01-14 21:15:20 +0100

    Create Streaming.js

---
+++ Streaming.js
@@ -0,0 +1,52 @@
+{
+    "name": "StreamingCommunity",
+    "version": "1.0.0",
+    "author": {
+        "name": "sobet",
+        "website": "https://google.com"
+    },
+    "iconURL": "https://raw.githubusercontent.com/bshar1865/MFS/refs/heads/main/assets/animeworld.png",
+    "stream": "MP4",
+    "language": "IT",
+    "extractor": "normal",
+    "module": [
+        {
+            "search": {
+                "url": "https://streamingcommunity.ooo/search",
+                "parameter": "q",
+                "documentSelector": "main div",
+                "title": "img.alt",
+                "image": {
+                    "url": "img",
+                    "attribute": "src"
+                },
+                "href": "a"
+            },
+            "featured": {
+                "url": "https://streamingcommunity.ooo",
+                "documentSelector": "div.content[data-name=all] div.item",
+                "title": "a.name",
+                "image": {
+                    "url": "img",
+                    "attribute": "src"
+                },
+                "href": "a.poster"
+            },
+            "details": {
+                "baseURL": "https://animeworld.so",
+                "aliases": {
+                    "selector": "div.widget-title h1",
+                    "attribute": "data-jtitle"
+                },
+                "synopsis": "div.info div.desc",
+                "airdate": "div.row dl.meta dt:contains(Data di Uscita) + dd",
+                "stars": "dd.rating span"
+            },
+            "episodes": {
+                "selector": "main div a.play",
+                "order": "ordered",
+                "pattern": ""
+            }
+        }
+    ]
+}
