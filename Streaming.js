e6a10624a7b1815b254104cd5f8eb4e855eaa460
Alex345-dev <36_tributo.chevrolet@icloud.com>
2025-01-16 16:00:52 +0100

    Update Streaming.js

--- Streaming.js
+++ Streaming.js
@@ -15,7 +15,7 @@
                 "url": "https://guardaserie.academy/&do=search&subaction=search",
                 "parameter": "story",
                 "documentSelector": ".mlnew",
-                "title": ".mlnh-thumb img",
+                "title": "mlnh-thumb a.title",
                 "image": {
                     "url": ".mlnh-thumb img",
                     "attribute": "src"
