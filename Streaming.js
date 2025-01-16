d7e8c545248142e30ddf3efa73929260218b9959
Alex345-dev <36_tributo.chevrolet@icloud.com>
2025-01-16 19:06:00 +0100

    Update Streaming.js

--- Streaming.js
+++ Streaming.js
@@ -20,7 +20,7 @@
                     "url": ".mlnh-thumb img",
                     "attribute": "src"
                 },
-                "href": "href"
+                "href": ".mlnh-2 a"
             },
             "featured": {
                 "url": "https://streamingcommunity.ooo",
