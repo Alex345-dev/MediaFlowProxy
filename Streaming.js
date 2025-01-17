c7f6c3cd64584a82506635c83175275a872d1d97
Alex345-dev <36_tributo.chevrolet@icloud.com>
2025-01-17 18:03:50 +0100

    Update Streaming.js

--- Streaming.js
+++ Streaming.js
@@ -12,8 +12,8 @@
     "module": [
         {
             "search": {
-                "url": "https://cb01.com.im/index.php?do=search",
-                "parameter": "do",
+                "url": "https://cb01.com.im/?do=search&subaction=search",
+                "parameter": "story",
                 "documentSelector": ".container .row .col-sm-8",
                 "title": ".short-main a",
                 "image": {
